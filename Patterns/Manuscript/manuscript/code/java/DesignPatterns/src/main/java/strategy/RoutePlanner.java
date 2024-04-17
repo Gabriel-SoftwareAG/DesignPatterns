3a056bd54ca0d9dba6dccdb24f39d1f1f4cd7dcf
Nir Gallner <nir@verisoft.ai>
2024-04-17 08:29:07 +0300

    strategy

--- Patterns/Manuscript/manuscript/code/java/DesignPatterns/src/main/java/strategy/RoutePlanner.java
+++ Patterns/Manuscript/manuscript/code/java/DesignPatterns/src/main/java/strategy/RoutePlanner.java
@@ -4,10 +4,12 @@ class RoutePlanner {
     private RouteStrategy strategy;
 
     public void setStrategy(RouteStrategy strategy) {
+        System.out.println("Setting the strategy to calculate the route: " + strategy.getClass().getSimpleName());
         this.strategy = strategy;
     }
 
     public void executeStrategy() {
+        System.out.println("Executing the strategy to calculate the route for " + strategy.getClass().getSimpleName());
         strategy.calculateRoute();
     }
 }
