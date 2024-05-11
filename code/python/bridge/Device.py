498a9c9530836308e06b432581022c23a2444655
Nir Gallner <nir@verisoft.ai>
2024-05-11 19:33:09 +0300

    small fixes

--- code/python/bridge/Device.py
+++ code/python/bridge/Device.py
@@ -1,12 +1,19 @@
-class Device:
+from abc import ABC, abstractmethod
+
+
+class Device(ABC):
+    @abstractmethod
     def power_on(self):
         pass
 
+    @abstractmethod
     def power_off(self):
         pass
 
+    @abstractmethod
     def adjust_volume(self, volume):
         pass
 
+    @abstractmethod
     def change_channel(self, channel):
         pass
